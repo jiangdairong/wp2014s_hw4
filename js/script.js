234914ee77a34aac9f3429585f2c74e4831e68f7
dairong <ambera23325a@gmail.com>
2014-06-07 01:33:11 +0800

    modify js

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ window.fbAsyncInit = function () {
                    
 				FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
 					var str="<img src="+ response.data.url +">";
-					$('#preview1').append(str);
-                    //$('#preview1').attr("src",response.data.url);
+					//$('#preview1').append(str);
+                    $('#preview1').attr("src",response.data.url);
 				});
 					
 					
