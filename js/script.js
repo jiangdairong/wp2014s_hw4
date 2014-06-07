e01a591c39749774b5205671cefdcf76bb2bb62a
dairong <ambera23325a@gmail.com>
2014-06-08 00:19:20 +0800

    modify

--- js/script.js
+++ js/script.js
@@ -205,7 +205,14 @@ function PostImageToFacebook(authToken) {
     }
 }
 
-
+//comment script
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=425373804271654&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 // Convert a data URI to blob把影像載入轉換函數
