7faae9c91f64cf9de0191d1ff8f4980ffe3e7542
dairong <ambera23325a@gmail.com>
2014-06-10 00:52:06 +0800

    modify

--- js/script.js
+++ js/script.js
@@ -123,13 +123,6 @@ window.fbAsyncInit = function () {
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
             ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-            var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
-            profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
-            
-            //canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
-            //canvas.height = profileIMG.height;
-            
-            ctx.drawImage(profileIMG,canMouseX-(profileIMG.width/2),canMouseY-(profileIMG.height/2));//從XY軸0，0值開始畫如profileimg
             ctx.drawImage(img,200,400); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
              if(uploaded === true){
                 ctx.drawImage(image4, 0 , 0 , image4.width, image4.height);
@@ -147,6 +140,14 @@ window.fbAsyncInit = function () {
             ctx.fillStyle = "black"; //字體顏色
             ctx.font='20px "微軟正黑體"'; //字體大小和字形
             ctx.fillText(inputedText, 200,400); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+            
+            var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
+            profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+            
+            //canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
+            //canvas.height = profileIMG.height;
+            
+            ctx.drawImage(profileIMG,canMouseX-(profileIMG.width/2),canMouseY-(profileIMG.height/2));//從XY軸0，0值開始畫如profileimg
       }
     }
 
